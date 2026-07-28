76c645c51564305627842da1bc650ab5eeb4ab25
André Moraes <dedehmoraes@hotmail.com>
2026-07-28 00:53:56 -0300

    Site para Teste Ness Tech Deploy

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+// Rola a página para o topo sempre que a rota muda.
+export default function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
